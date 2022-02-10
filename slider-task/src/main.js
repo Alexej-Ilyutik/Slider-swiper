6c0139f9ccf1b8a1afab6860de1de150d54a4770
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-02-10 23:57:13 +0300

    feat: add slider

--- slider-task/src/main.js
+++ slider-task/src/main.js
@@ -5,3 +5,50 @@ import './styles/all.scss'
 
 // document.querySelector('body').appendChild(component)
 
+const sliderContainer = document.querySelector('.slider-container');
+const items = document.querySelectorAll('.slider__image');
+const prevBtn = document.querySelector('.slider__prev');
+const nextBtn = document.querySelector('.slider__next');
+
+let counter = 1;
+const size = items[0].clientWidth;
+
+function changePosition(){
+sliderContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+
+changePosition();
+
+function changeTransition() {
+  sliderContainer.style.transition = '0.4s ease-in-out';
+}
+
+prevBtn.addEventListener('click', () =>{
+  if(counter <= 0) return;
+  changeTransition();
+  counter--;
+  changePosition();
+})
+
+nextBtn.addEventListener('click', () => {
+   if (counter >= items.length - 1) return;
+  changeTransition();
+  counter++;
+  changePosition();
+});
+
+
+sliderContainer.addEventListener('transitionend', ()=>{
+   if(items[counter].id === 'lastClone'){
+    sliderContainer.style.transition = "none";
+     counter = items.length-2;
+     changePosition();
+   }
+   if (items[counter].id === 'firstClone') {
+     sliderContainer.style.transition = 'none';
+     counter = items.length - counter;
+     changePosition();
+   }
+})
+
+console.log(size);
