c942dd0c99e8c78278ca3a4d280a523497b12558
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-02-11 11:25:52 +0300

    fix: change slider add adaptive

--- slider-task/src/main.js
+++ slider-task/src/main.js
@@ -1,4 +1,4 @@
-import './styles/all.scss'
+import './styles/all.scss';
 // import MainComponent from './components/MainComponent'
 
 // const component = MainComponent('My compoent text')
@@ -11,44 +11,55 @@ const prevBtn = document.querySelector('.slider__prev');
 const nextBtn = document.querySelector('.slider__next');
 
 let counter = 1;
+let width;
 const size = items[0].clientWidth;
 
-function changePosition(){
-sliderContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+function init() {
+  width = document.querySelector('.carousel').offsetWidth;
+  sliderContainer.style.width = width*items.length + 'px';
+  items.forEach((item) => {
+    item.style.width = width + 'px';
+    item.style.height = 'auto';
+  });
+  changePosition();
 }
 
-changePosition();
+window.addEventListener('resize', init);
+init();
+
+function changePosition(){
+   sliderContainer.style.transform = 'translate(-'+counter*width+'px)'
+}
 
 function changeTransition() {
   sliderContainer.style.transition = '0.4s ease-in-out';
 }
 
-prevBtn.addEventListener('click', () =>{
-  if(counter <= 0) return;
+prevBtn.addEventListener('click', () => {
+  if (counter <= 0) return;
   changeTransition();
   counter--;
   changePosition();
-})
+});
 
 nextBtn.addEventListener('click', () => {
-   if (counter >= items.length - 1) return;
+  if (counter >= items.length - 1) return;
   changeTransition();
   counter++;
   changePosition();
 });
 
-
-sliderContainer.addEventListener('transitionend', ()=>{
-   if(items[counter].id === 'lastClone'){
-    sliderContainer.style.transition = "none";
-     counter = items.length-2;
-     changePosition();
-   }
-   if (items[counter].id === 'firstClone') {
-     sliderContainer.style.transition = 'none';
-     counter = items.length - counter;
-     changePosition();
-   }
-})
+sliderContainer.addEventListener('transitionend', () => {
+  if (items[counter].id === 'lastClone') {
+    sliderContainer.style.transition = 'none';
+    counter = items.length - 2;
+    changePosition();
+  }
+  if (items[counter].id === 'firstClone') {
+    sliderContainer.style.transition = 'none';
+    counter = items.length - counter;
+    changePosition();
+  }
+});
 
 console.log(size);
